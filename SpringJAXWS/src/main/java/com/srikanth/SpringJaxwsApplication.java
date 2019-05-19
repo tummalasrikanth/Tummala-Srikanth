51f5a205ae2101d5a003d7366bd6f5538a820f4f
HP pc <HP pc@DESKTOP-5ECA84B>
2019-05-19 17:54:47 +0530

    First commit

---
+++ SpringJAXWS/src/main/java/com/srikanth/SpringJaxwsApplication.java
@@ -0,0 +1,13 @@
+package com.srikanth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJaxwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJaxwsApplication.class, args);
+	}
+
+}
