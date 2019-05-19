5fb5187934e5d0e33d16bc3139b64fda9a142e80
HP pc <HP pc@DESKTOP-5ECA84B>
2019-05-19 17:51:30 +0530

    First commit

---
+++ JAXWSSpringBoot/src/test/java/com/srikanth/SpringJaxwsServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.srikanth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJaxwsServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
